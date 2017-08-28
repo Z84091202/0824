5e73ba351874e8e3cf0d26453122f5574110a2e6
ZX <Z84091202@gmail.com>
2017-08-29 06:07:04 +0800

    側拉式介面，主程式以改2

--- app/src/main/java/com/qiniu/pili/droid/streaming/demo/G26/MainActivity.java
+++ app/src/main/java/com/qiniu/pili/droid/streaming/demo/G26/MainActivity.java
@@ -135,11 +135,26 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
         }
 
-        return super.onOptionsItemSelected(item);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+
+
+        //return super.onOptionsItemSelected(item);
     }
 /*
     @SuppressWarnings("StatementWithEmptyBody")
